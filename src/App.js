641ab6c07029360f98de02956be847c0e3742dda
chinoaladino <41176183+chinoaladino@users.noreply.github.com>
2020-06-22 18:04:26 -0400

    Update App.js
    seba comelax2

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" >
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+          </form>
+        </div>      
+      </nav>
+      <div className="container mt-5">
+        <span className="display-4">TITULO </span>
+      </div>
+    </div>
     </div>
   );
 }
