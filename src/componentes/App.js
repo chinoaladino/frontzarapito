25b09ad0931950f1a2be36fc126fdb6a33d161ab
Noaa1 <Noaa1@users.noreply.github.com>
2020-06-24 17:06:20 -0400

    orden

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel, Ul , Container } from 'react-bootstrap'
 
