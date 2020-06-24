5a22b45452c6acd388fc8d5d20d5f17b94c693dd
Noaa1 <Noaa1@users.noreply.github.com>
2020-06-24 18:33:24 -0400

    actualizacion

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -120,39 +120,24 @@ function App() {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="/img/imagen1.webp"
+              src="/imagenes/3.jpg"
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
+          
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="/img/imagen2.jpg"
+              src="/imagenes/2.webp"
               alt="Third slide"
             />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="/img/imagen3.jpg"
+              src="/imagenes/1.jpg"
               alt="Third slide"
             />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </Container>
